66b1a20de22d48268bea67e3fa3b30b41b651eff
Rafa <rafacv23@gmail.com>
2025-05-04 21:37:00 +0200

    Add Navigation component with footer links and icons

---
+++ components/Navigation.tsx
@@ -0,0 +1,44 @@
+import { Barcode, Boxes, House, PackageCheck } from "lucide-react"
+
+export default function Navigation() {
+  return (
+    <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <House />
+        Inicio
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Barcode />
+        Escaner
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <PackageCheck />
+        Pedido
+      </a>
+      <a
+        className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+        href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Boxes />
+        Almacén
+      </a>
+    </footer>
+  )
+}
