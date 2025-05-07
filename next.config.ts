80b086fa34f734b223189b59ef75ddac9324404e
Rafa Canosa <rafacv23@gmail.com>
2025-05-07 14:24:16 +0200

    Add new image assets for Windows 11 tiles and logos in various scales
    - Added LargeTile images at scale 100, 125, 150, 200, and 400
    - Added SmallTile images at scale 100, 125, 150, 200, and 400
    - Added SplashScreen images at scale 100, 125, 150, 200, and 400
    - Added Square150x150Logo images at scale 100, 125, 150, 200, and 400
    - Added Square44x44Logo images with various target sizes and scales
    - Added StoreLogo images at scale 100, 125, 150, 200, and 400
    - Added Wide310x150Logo images at scale 100, 125, 150, 200, and 400

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const withPWA = require("next-pwa")({
   dest: "public",
-  register: true,
-  skipWaiting: true,
 })
 
-const nextConfig = {
-  reactStrictMode: true, // This ends up in the wrong place!
-}
-
-module.exports = withPWA(nextConfig)
+module.exports = withPWA({
+  reactStrictMode: true,
+})
