678b383f1a5ba104ad5771b4e93bf51002f945ee
Rafa Canosa <rafacv23@gmail.com>
2025-05-20 01:19:03 +0200

    Add formatString utility to capitalize product names

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// format a string of lowercases letters, the first letter to uppercase and the rest to lowercase
+export function formatString(productName: string): string {
+  return (
+    productName.charAt(0).toUpperCase() + productName.slice(1).toLowerCase()
+  )
+}
